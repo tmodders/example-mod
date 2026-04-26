3bfe212c9969534a99571a0eca7b804f69bf74b8
shnakamura <joaovtnakamura@gmail.com>
2026-04-26 08:01:32 -0300

    feat: basic sword example

---
+++ Content/Swords/ExampleSword.cs
@@ -0,0 +1,34 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Swords;
+
+/// <summary>
+///     Provides a practical example of a sword.
+/// </summary>
+public class ExampleSword : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.DamageType = DamageClass.Melee;
+        
+        Item.damage = 10;
+        Item.knockBack = 1f;
+
+        Item.autoReuse = true;
+
+        // Matches the dimensions of the item's texture.
+        Item.width = 16;
+        Item.height = 16;
+        
+        Item.UseSound = SoundID.Item1;
+
+        // Indicates the item takes 20 frames to be used.
+        Item.useTime = 20;
+        
+        // Indicates the animation of the item lasts 20 frames.
+        Item.useAnimation = 20;
+
+        Item.useStyle = ItemUseStyleID.Swing;
+    }
+}
